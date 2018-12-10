3b09007a8b44fcd7206e32e14f33c24907e17d1f
Yidong Cui <cyd@bupt.edu.cn>
2018-12-10 09:27:26 +0800

    Enable buffers exchange between windows

---
+++ my-buffer/layers.el
@@ -0,0 +1,12 @@
+;; This file is the first file to be loaded and this is the place where
+;; addtional layers can be declared.
+
+;; For instance is layer A depends on some functionality of layer B then in the
+;; file layers.el of layer A we can add:
+;;     (configuration-layer/declare-layer 'B)
+;; The effect is that B is considered a used layer and will be loaded as if it
+;; was added to dotspacemacs-configuration-layers variables.
+
+
+;; cyd@20181111 We have nothing here
+;; This file is just a stake holder
--- my-commons/config-shell.el
+++ my-commons/config-shell.el
@@ -4,6 +4,17 @@
 ;; Cui Yidong <nathan.cui@gmail.com>
 ;; Rev: 20160730, 20180609
 
+;; !!!!!!!! BE AWARE !!!!!!!!
+;; When using lsp-haskell, hie-wrapper is invoked with the following shell
+;; One must check if hie-wrapper is running smoothly under emacs shell
+;;    TO TEST
+;; M-x shell
+;; hie-wrapper --lsp -d -l C:/temp/hie.log
+;;    Then see c:/temp/hie.log
+
+;; !!!!!!!! BE AWARE !!!!!!!!
+;; When updating OS path, delete ~/.spacemacs.env
+;; OR you will not see the path in emacs being updated!!!
 
 ;; ==================================================
 ;; Change the default shell from cmd.exe to msys2
@@ -15,6 +26,7 @@
       (setq explicit-shell-file-name "C:/msys64/usr/bin/zsh.exe")
       (setq shell-file-name "zsh")
       ;; (setq explicit-bash.exe-args '("--noediting" "--login" "-i"))
+      ;; (setq explicit-zsh.exe-args '("--emacs" "--login"))
       (setenv "SHELL" shell-file-name)
       ;; remove the input Ctrl-M character
       (add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m)
